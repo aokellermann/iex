fa42a4a5b252a551951899c4169c3bbc7c832d2d
Antony Kellermann <aokellermann@gmail.com>
2020-05-19 17:25:42 -0400

    Added a templated ErrorCode contructor.

--- iex/iex.h
+++ iex/iex.h
@@ -53,15 +53,20 @@ class ErrorCode : public std::string
   explicit ErrorCode(const NamedPair<ErrorCode>& named_ec) : ErrorCode(named_ec.first, named_ec.second) {}
 
   ErrorCode(const std::string& message, const std::initializer_list<NamedPair<ErrorCode>>& named_ec_list)
-      : std::string(message)
+      : ErrorCode(message, named_ec_list.begin(), named_ec_list.end())
   {
-    if (named_ec_list.size() != 0)
+  }
+
+  template <class InputIt>
+  ErrorCode(const std::string& message, InputIt named_ecs_begin, InputIt named_ecs_end) : std::string(message)
+  {
+    if (named_ecs_begin != named_ecs_end)
     {
       append(": [");
 
-      for (const auto& named_ec : named_ec_list)
+      for (auto iter = named_ecs_begin; iter != named_ecs_end; ++iter)
       {
-        append(ErrorCode(named_ec) + ", ");
+        append(ErrorCode(*iter) + ", ");
       }
       pop_back();
       pop_back();
