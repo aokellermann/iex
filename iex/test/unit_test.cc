5382773c26ae13763cf43fe1207ab4b952b429ae
Antony Kellermann <aokellermann@gmail.com>
2020-05-18 23:51:02 -0400

    Singleton (#19)
    ### Changes
    * Adds singleton::GetInstance<T>() function for a templatized version of the singleton software pattern.
    * Adds multi-threaded testing to ensure uniqueness.
    * Closes #18.
    
    ### Additional Context
    A base class with manual synchronization was actually not needed in this case. Since C++11, you can use [static local variables](https://en.cppreference.com/w/cpp/language/storage_duration#Static_local_variables).

--- iex/test/unit_test.cc
+++ iex/test/unit_test.cc
@@ -15,13 +15,63 @@
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include <gtest/gtest.h>
+#include <mutex>
+#include <thread>
+#include <unordered_set>
+#include <vector>
+
+#include <gtest/gtest.h>  // NOLINT Why does linter think this is a C library?
 #include "iex/iex.h"
+#include "iex/singleton.h"
 
 TEST(iex, init_test)
 {
   const auto ec = iex::Init();
   EXPECT_TRUE(ec.Success());
+}
+
+/**
+ * Test whether singleton works properly.
+ * Creates a bunch of the same class using singleton::GetInstance(), but each with a different identifier.
+ * Then, puts them into a set, using their id as a hash.
+ * If singleton works properly, the set will be of size 1.
+ */
+TEST(iex, singleton_unique)
+{
+  struct SingletonImpl
+  {
+    int i;
+    bool operator==(const SingletonImpl& other) const { return i == other.i; }
+  };
+
+  struct SingletonHasher
+  {
+    std::size_t operator()(const SingletonImpl& s) const { return std::hash<int>()(s.i); }
+  };
+
+  std::unordered_set<SingletonImpl, SingletonHasher> set;
+  std::mutex mutex;
+
+  const auto thread_func = [&set, &mutex](int i) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Try to induce pileup
+    const auto& instance = iex::singleton::GetInstance<SingletonImpl>();
+    std::lock_guard<std::mutex> lock(mutex);
+    set.insert(instance);
+  };
+
+  const std::size_t num_threads = 1000;
+  std::vector<std::thread> threads(num_threads);
+  for (int i = 0; i < num_threads; ++i)
+  {
+    threads[i] = std::thread(thread_func, i);
+  }
+
+  for (int i = 0; i < num_threads; ++i)
+  {
+    threads[i].join();
+  }
+
+  EXPECT_EQ(set.size(), 1);
 }
 
 int main(int argc, char** argv)
