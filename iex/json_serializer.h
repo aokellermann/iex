268d8da61057391b6bfd3987980c5d7acb213b27
Antony Kellermann <aokellermann@gmail.com>
2020-05-25 23:14:29 -0400

    Add base class to JsonSerializer classes and remove some accessibility modifiers

--- iex/json_serializer.h
+++ iex/json_serializer.h
@@ -17,36 +17,40 @@ namespace iex::json
 {
 using Json = nlohmann::json;
 
+class JsonBase
+{
+ protected:
+  Json repr_;
+};
+
 /**
  * This is an abstract interface for objects that can be serialized with Json data.
  */
-class JsonSerializable
+struct JsonSerializable : virtual JsonBase
 {
- protected:
   /**
    * Creates and returns a Json object representing this object. This function must be overridden.
    * @return Json
    */
-  virtual ValueWithErrorCode<Json> Serialize() = 0;
+  virtual ErrorCode Serialize() = 0;
 };
 
 /**
  * This is an abstract interface for objects that can be deserialized with Json data.
  */
-class JsonDeserializable
+struct JsonDeserializable : virtual JsonBase
 {
- protected:
   /**
    * Stores data from input_json in this object. This function must be overridden.
    * @return ErrorCode if failure
    */
-  virtual ErrorCode Deserialize(const Json& input_json) = 0;
+  virtual ErrorCode Deserialize() = 0;
 };
 
 /**
  * This is an abstract interface for objects that can be both serialized and deserialized with Json data.
  */
-class JsonBidirectionalSerializable : protected JsonSerializable, protected JsonDeserializable
+struct JsonBidirectionalSerializable : JsonSerializable, JsonDeserializable
 {
 };
 
